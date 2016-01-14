39a114f791b260095694e82f939b1945f4e48d85
robbiest <robbiest@hotmail.com>
2016-01-14 19:33:24 +0000

    Move files around

--- mkyong/spring/mavenspringhelloworld/SpringExamples/src/main/java/com/mkyong/common/App.java
+++ mkyong/spring/mavenspringhelloworld/SpringExamples/src/main/java/com/mkyong/common/App.java
@@ -1,13 +1,14 @@
 package com.mkyong.common;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Worlden!" );
-    }
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"Spring-Module.xml");
+
+		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+		obj.printHello();
+	}
 }
