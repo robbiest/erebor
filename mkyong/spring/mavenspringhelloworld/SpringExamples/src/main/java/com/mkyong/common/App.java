63f2e9c725910eeb9a65cdcf3bddc5f7bd30506d
robbiest <robbiest@hotmail.com>
2016-01-14 20:43:37 +0000

    First commit

--- mkyong/spring/mavenspringhelloworld/SpringExamples/src/main/java/com/mkyong/common/App.java
+++ mkyong/spring/mavenspringhelloworld/SpringExamples/src/main/java/com/mkyong/common/App.java
@@ -1,6 +1,7 @@
 package com.mkyong.common;
 
 import org.springframework.context.ApplicationContext;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
