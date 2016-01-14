a3261e3d6fd242f4d2432708dd422499ded3660c
robbiest <robbiest@hotmail.com>
2016-01-14 21:32:22 +0000

    Java Commit

---
+++ mkyong/spring/BeanBasic/SpringInnerBeanExample/src/main/java/com/mkyong/common/Customer.java
@@ -0,0 +1,19 @@
+package com.mkyong.common;
+
+public class Customer 
+{
+	private Person person;
+
+	public Customer(Person person) {
+		this.person = person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [person=" + person + "]";
+	}
+}
