554fd6cd149229d3b2f7fc42527f24a2deecf4da
robbiest <robbiest@hotmail.com>
2016-01-15 13:31:05 +0000

    Com

---
+++ mkyong/spring/ExpressionLanguage/ELBeanReference/src/main/java/com/mkyong/core/Customer.java
@@ -0,0 +1,50 @@
+package com.mkyong.core;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("customerBean")
+public class Customer {
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getFullAddress() {
+		return fullAddress;
+	}
+
+	public void setFullAddress(String fullAddress) {
+		this.fullAddress = fullAddress;
+	}
+
+	@Value("#{addressBean}")
+	private Address address;
+
+	@Value("#{addressBean.country}")
+	private String country;
+	
+	@Value("#{addressBean.getFullAddress('mkyong')}")
+	private String fullAddress;
+
+	//getter and setter methods
+	
+	@Override
+	public String toString() {
+		return "Customer [address=" + address + "\n, country=" + country
+				+ "\n, fullAddress=" + fullAddress + "]";
+	}
+
+}
---
+++ mkyong/spring/ExpressionLanguage/ELMethodInvocation/src/main/java/com/mkyong/core/App.java
@@ -0,0 +1,23 @@
+package com.mkyong.core;
+ 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
+public class App {
+	public static void main(String[] args) {
+
+	    
+	    //ApplicationContext context = new ClassPathXmlApplicationContext("EquivalentBean.xml");
+	    
+	    //Customer obj = (Customer) context.getBean("customerBean");
+	    //System.out.println(obj);
+
+	    
+	    ApplicationContext other = new ClassPathXmlApplicationContext("SpringBeansAnno.xml");
+	    
+	    Customer ooo = (Customer) other.getBean("customerBean");
+	    System.out.println(ooo);	    
+	    
+
+	}
+}
