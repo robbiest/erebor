6d55a46718367526b6d8d8f733ffb21e7192e5cf
robbiest <robbiest@hotmail.com>
2016-01-16 12:29:42 +0000

    Com

---
+++ mkyong/spring/HibernateSupport/MavenSpringHibernateAnnotationMySqlExample/src/main/java/com/mkyong/stock/model/Stock.java
@@ -0,0 +1,75 @@
+package com.mkyong.stock.model;
+
+import javax.persistence.Column;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.SequenceGenerator;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "stock", catalog = "hib", uniqueConstraints = {
+		@UniqueConstraint(columnNames = "STOCK_NAME"),
+		@UniqueConstraint(columnNames = "STOCK_CODE") })
+public class Stock implements java.io.Serializable {
+
+	private Integer stockId;
+	private String stockCode;
+	private String stockName;
+
+	public Stock() {
+	}
+
+	public Stock(String stockCode, String stockName) {
+		this.stockCode = stockCode;
+		this.stockName = stockName;
+	}
+
+	@Id
+	//@GeneratedValue(strategy = IDENTITY)
+	
+	@SequenceGenerator(name="MY_SEQ", sequenceName="STOCK_SEQ")
+	//@GeneratedValue(strategy = GenerationType.SEQUENCE ,generator="my_seq")
+	
+	
+	@GeneratedValue(strategy=SEQUENCE, generator="MY_SEQ")
+	@Column(name = "STOCK_ID", unique = true, nullable = false)
+	public Integer getStockId() {
+		return this.stockId;
+	}
+
+	public void setStockId(Integer stockId) {
+		this.stockId = stockId;
+	}
+
+	@Column(name = "STOCK_CODE", unique = true, nullable = false, length = 10)
+	public String getStockCode() {
+		return this.stockCode;
+	}
+
+	public void setStockCode(String stockCode) {
+		this.stockCode = stockCode;
+	}
+
+	@Column(name = "STOCK_NAME", unique = true, nullable = false, length = 20)
+	public String getStockName() {
+		return this.stockName;
+	}
+
+	public void setStockName(String stockName) {
+		this.stockName = stockName;
+	}
+
+	@Override
+	public String toString() {
+		return "Stock [stockCode=" + stockCode + ", stockId=" + stockId
+				+ ", stockName=" + stockName + "]";
+	}
+}
