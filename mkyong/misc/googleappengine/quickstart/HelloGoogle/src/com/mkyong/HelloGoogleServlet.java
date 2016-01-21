6ef3ef5e288b9a11606981a842fba3547967f71d
robbiest <robbiest@hotmail.com>
2016-01-21 17:59:32 +0000

    c

---
+++ mkyong/misc/googleappengine/quickstart/HelloGoogle/src/com/mkyong/HelloGoogleServlet.java
@@ -0,0 +1,12 @@
+package com.mkyong;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HelloGoogleServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
